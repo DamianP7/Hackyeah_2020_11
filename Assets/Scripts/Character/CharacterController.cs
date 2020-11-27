ffe0239bc690c08419e8d652c5d214c3dfd6b585
DamianP7 <damian7it@gmail.com>
2020-11-27 22:02:41 +0100

    Player Controller added

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+	public Transform padPointer;
+	public Transform mousePointer;
+	public float maxPointerDistance;
+
+	[SerializeField] float speed = 1;
+
+	int hp = 100;
+	Rigidbody2D rb;
+	Transform currentPointer;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+
+	public void Aim(Vector2 direction, bool isMouse = false)
+	{
+		padPointer.gameObject.SetActive(!isMouse);
+		mousePointer.gameObject.SetActive(isMouse);
+
+		if (isMouse)
+		{
+			currentPointer = mousePointer;
+
+			currentPointer.position = direction;
+		}
+		else
+		{
+			currentPointer = padPointer;
+			currentPointer.localPosition = direction * maxPointerDistance;
+
+		}
+	}
+
+	public void Move(Vector2 direction)
+	{
+		Vector3 pos = new Vector3(direction.x * speed, direction.y * speed) + rb.transform.position;
+		rb.MovePosition(pos);
+	}
+
+	public void Shoot()
+	{
+
+	}
+
+	void MovePointer(Vector2 position, bool isMouse)
+	{
+		padPointer.gameObject.SetActive(!isMouse);
+		mousePointer.gameObject.SetActive(isMouse);
+
+		if (isMouse)
+			currentPointer = mousePointer;
+		else
+			currentPointer = padPointer;
+
+		currentPointer.position = position;
+	}
+
+}
---
+++ Assets/Scripts/ControlManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlManager : MonoBehaviour
+{
+	public CharacterController player;
+
+
+	bool left, right, up, down;
+	bool gamepadInput;
+
+	public enum Control
+	{
+		PC,
+		Controller,
+		Mobile
+	}
+
+#if UNITY_ANDROID
+	Control controls = Control.Mobile;
+#else
+	public Control controls = Control.Controller;
+#endif
+
+	void Update()
+	{
+		if (controls == Control.Mobile)
+			return;
+		else if (Gamepad.current != null && controls == Control.Controller)
+			HandleGamepad();
+		else if (controls == Control.PC)
+			HandlePC();
+	}
+
+	void HandleGamepad()
+	{
+		var gamepad = Gamepad.current;
+		if (gamepad == null)
+			return; // No gamepad connected.
+
+		if (gamepad.rightTrigger.wasPressedThisFrame)
+		{
+			// TODO: full auto fire
+			player.Shoot();
+		}
+		if (gamepad.rightShoulder.wasPressedThisFrame)
+		{
+			//player.NextWeapon();
+		}
+		if (gamepad.leftShoulder.wasPressedThisFrame)
+		{
+			//player.PreviousWeapon();
+		}
+
+		player.Aim(gamepad.rightStick.ReadValue());
+
+		player.Move(gamepad.leftStick.ReadValue());
+	}
+
+	void HandlePC()
+	{
+		if (Mouse.current.leftButton.wasPressedThisFrame)
+		{
+			player.Shoot();
+		}
+		//if (Mouse.current.scroll.)
+		//{
+		//	//player.NextWeapon();
+		//}
+		//if (gamepad.leftShoulder.wasPressedThisFrame)
+		//{
+		//	//player.PreviousWeapon();
+		//}
+		player.Aim(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()),true);
+
+
+		if (Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.aKey.wasPressedThisFrame)
+			left = true;
+		else if (Keyboard.current.leftArrowKey.wasReleasedThisFrame || Keyboard.current.aKey.wasReleasedThisFrame)
+			left = false;
+
+		if (Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.dKey.wasPressedThisFrame)
+			right = true;
+		else if (Keyboard.current.rightArrowKey.wasReleasedThisFrame || Keyboard.current.dKey.wasReleasedThisFrame)
+			right = false;
+
+		if (Keyboard.current.upArrowKey.wasPressedThisFrame || Keyboard.current.wKey.wasPressedThisFrame)
+			up = true;
+		else if (Keyboard.current.upArrowKey.wasReleasedThisFrame || Keyboard.current.wKey.wasReleasedThisFrame)
+			up = false;
+
+		if (Keyboard.current.downArrowKey.wasPressedThisFrame || Keyboard.current.sKey.wasPressedThisFrame)
+			down = true;
+		else if (Keyboard.current.downArrowKey.wasReleasedThisFrame || Keyboard.current.sKey.wasReleasedThisFrame)
+			down = false;
+
+		Vector2 direction = Vector2.zero;
+		if (up)
+			direction.y += 1;
+		if (down)
+			direction.y -= 1;
+		if (left)
+			direction.x -= 1;
+		if (right)
+			direction.x += 1;
+
+		player.Move(direction);
+	}
+
+
+}
