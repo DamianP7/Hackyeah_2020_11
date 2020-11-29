128119c564c67c3bdd5d5a010af340a25ae6e981
DamianP7 <damian7it@gmail.com>
2020-11-29 09:51:24 +0100

    OpenGL update

---
+++ Assets/Scripts/EnableIfPlatform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableIfPlatform : MonoBehaviour
+{
+    public GameObject gameObj;
+
+    private void Awake()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        gameObj.SetActive(false);
+#endif
+    }
+}
